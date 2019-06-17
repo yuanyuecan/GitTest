bfccf5d12b16135951961ad754d01d84b57414a2
yuanyuecan <2767920740@qq.com>
2019-06-17 15:56:01 +0800

    张三添加了 fun2()

--- src/aaa/test.java
+++ src/aaa/test.java
@@ -4,4 +4,10 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("234567");
 	}
+	public void fun(){
+		System.out.println("23456767865432");
+	}
+	public void fun2(){
+		System.out.println("张三添加了fun2()");
+	}
 }
